1d32e1c9106fdb8202fe17bf4dcb84f72b3937da
DimsonGo <i@bolgov-1.ru>
2024-05-13 20:49:42 +0300

    Исправил

--- src/Main.java
+++ src/Main.java
@@ -13,16 +13,16 @@ public class Main {
         System.out.println("Задача 2");
         int[] paymentsMinMax = {12_567, 43_321, 21_000, 56_550, 45_000};
         int maxSum = -1;
-        int mimSum = 456_000;
+        int minSum = 456_000;
         for (int i = 0; i < paymentsMinMax.length; i++) {
             if (paymentsMinMax[i] > maxSum) {
                 maxSum = paymentsMinMax[i];
             }
-            if (paymentsMinMax[i] < mimSum) {
-                mimSum = paymentsMinMax[i];
+            if (paymentsMinMax[i] < minSum) {
+                minSum = paymentsMinMax[i];
             }
         }
-        System.out.println("Минимальная сумма за месяц " + mimSum + ". А максимальная сумма за месяц " + maxSum);
+        System.out.println("Минимальная сумма за месяц " + minSum + ". А максимальная сумма за месяц " + maxSum);
 
 
         System.out.println();
@@ -32,7 +32,7 @@ public class Main {
         for (int i = 0; i < money.length; i++) {
             suma = summa + money[i];
         }
-        double average = (double) suma / 20;
+        double average = (double) suma / money.length;
         System.out.println("Средняя сумма трат за месяц составила " + average);
 
         System.out.println();
